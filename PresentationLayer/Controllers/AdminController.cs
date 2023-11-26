957c2da7cdab927f1994f8ed33a6eb6ee295d503
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-11-26 05:02:12 +0300

    26.11.23

---
+++ PresentationLayer/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+
+        public AdminController(ICategoryService categoryService, IProductService productService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            _categoryService.TInsert(category);
+            return RedirectToAction("MainPage/Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var values = _categoryService.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _categoryService.TUpdate(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var values = _categoryService.TGetById(id);
+            _categoryService.TDelete(values);
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
