15ccdea6017cbf7181c6d4ab06dc18bbd46d4dcc
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-11-27 03:45:04 +0300

    27.11.23

--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace EntityLayer.Concrete
     public class Category
     {
         public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public string CategoryDescription { get; set; }
-        public string CategoryImage { get; set; }
+        [Required(ErrorMessage ="Kategori adını giriniz.")]
+        public string? CategoryName { get; set; }
+        public string? CategoryDescription { get; set; }
+
+        public string CategoryImage { get; set; } = string.Empty;
         public bool CategoryIsActive { get; set; }
     }
 }
--- PresentationLayer/Controllers/AdminController.cs
+++ PresentationLayer/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
 ﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace PresentationLayer.Controllers
 {
@@ -8,11 +11,13 @@ namespace PresentationLayer.Controllers
     {
         private readonly ICategoryService _categoryService;
         private readonly IProductService _productService;
+        private readonly ICategoryDal _categoryDal;
 
-        public AdminController(ICategoryService categoryService, IProductService productService)
+        public AdminController(ICategoryService categoryService, IProductService productService, ICategoryDal categoryDal)
         {
             _categoryService = categoryService;
             _productService = productService;
+            _categoryDal = categoryDal;
         }
 
         public IActionResult Index()
@@ -33,10 +38,32 @@ namespace PresentationLayer.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddCategory(Category category)
+        public async Task<IActionResult> AddCategory(Category category, IFormFile imageFile)
         {
-            _categoryService.TInsert(category);
-            return RedirectToAction("MainPage/Index");
+            var allowedExtensions = new[] { ".jpg", ".jpeg", "png" };
+            var extension = Path.GetExtension(imageFile.FileName);
+            var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+
+            if(imageFile != null)
+            {
+                if (!allowedExtensions.Contains(extension))
+                {
+                    ModelState.AddModelError("", "Geçerli bir resim türü seçiniz.");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                using(var stream=new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+                category.CategoryImage = randomFileName;
+                _categoryService.TInsert(category);
+                return RedirectToAction("CategoryList");
+            }
+            return View(category);
         }
 
 
@@ -54,7 +81,15 @@ namespace PresentationLayer.Controllers
             return RedirectToAction("CategoryList");
         }
 
+        [HttpGet]
         public IActionResult DeleteCategory(int id)
+        {
+            var values = _categoryService.TGetById(id);
+            return View("CategoryDeleteConfirm", values);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCategory(int id, int CategoryId)
         {
             var values = _categoryService.TGetById(id);
             _categoryService.TDelete(values);
